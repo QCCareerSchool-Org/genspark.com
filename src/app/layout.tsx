162a1183eb29f09d5892f5d2351af7b27587d194
Dave Welsh <dave@qccareerschool.com>
2023-03-15 15:27:29 -0400

    Remove extra vierport meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,9 +22,6 @@ import Image from 'next/image';
 export default function RootLayout({ children }: Props) {
   return (
     <html lang="en" className={openSans.className}>
-      <head>
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-      </head>
       <body>
         <header className={style.header}>
           <div className="wrapper">
